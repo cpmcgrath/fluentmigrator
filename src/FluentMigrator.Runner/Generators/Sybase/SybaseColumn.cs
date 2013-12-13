86196e1c52a3bd3753b988e672010e66ceac2277
Michael Anderson <mikeand654@gmail.com>
2013-12-13 20:59:24 +1000

    Add a sybase column type
    Column type has defaults as well as auto increment settings

---
+++ src/FluentMigrator.Runner/Generators/Sybase/SybaseColumn.cs
@@ -0,0 +1,41 @@
+﻿using FluentMigrator.Model;
+using FluentMigrator.Runner.Generators.Base;
+using FluentMigrator.Runner.Generators.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FluentMigrator.Runner.Generators.Sybase
+{
+    internal class SybaseColumn : ColumnBase
+    {
+        public SybaseColumn() : base(new SybaseTypeMap(), new GenericQuoter())
+        {
+        }
+
+
+        protected override string FormatIdentity(ColumnDefinition column)
+        {
+            return column.IsIdentity ? "DEFAULT AUTOINCREMENT" : string.Empty;
+        }
+
+        protected override string FormatSystemMethods(SystemMethods systemMethod)
+        {
+            switch (systemMethod)
+            {
+                case SystemMethods.CurrentDateTime:
+                    return "GETDATE()";
+                case SystemMethods.CurrentUser:
+                    return "CURRENT_USER";
+                case SystemMethods.CurrentUTCDateTime:
+                    return "CURRENT UTC TIMESTAMP";
+                case SystemMethods.NewGuid:
+                    return "NEWID()";
+                case SystemMethods.NewSequentialId:
+                    return "NEWID()";
+            }
+            return null;
+        }
+    }
+}
